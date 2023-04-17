d686e90105e8914cdd59ac468dafb948d23a8b21
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-04-17 19:40:03 +0000

    implementation of person's CRUD

--- src/main/java/com/eburtis/tp/domains/personne/Personne.java
+++ src/main/java/com/eburtis/tp/domains/personne/Personne.java
@@ -6,8 +6,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.json.JSONObject;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name = "personne")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Personne {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +25,16 @@ public class Personne {
     private String nom;
 
     private String prenoms;
-    
+
     private Long age;
+
+    public JSONObject getJson() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", this.id);
+        jsonObject.put("nom", this.nom);
+        jsonObject.put("prenoms", this.prenoms);
+        jsonObject.put("age", this.age);
+        return jsonObject;
+    }
+    
 }
--- src/main/java/com/eburtis/tp/infrastructure/PersonneServiceImpl.java
+++ src/main/java/com/eburtis/tp/infrastructure/PersonneServiceImpl.java
@@ -1,11 +1,17 @@
 package com.eburtis.tp.infrastructure;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.eburtis.tp.application.personne.PersonneService;
 import com.eburtis.tp.application.personne.PersonneVO;
+import com.eburtis.tp.domains.personne.Personne;
 import com.eburtis.tp.domains.personne.PersonneRepository;
 
 @Service
@@ -17,32 +23,101 @@ public class PersonneServiceImpl implements PersonneService {
     @Override
     public String savePersonne(PersonneVO personneVo) {
         JSONObject jsonObject = new JSONObject();
-        return jsonObject.toString();
+        Personne personne = new Personne();
 
+        personne.setNom(personneVo.getNom());
+        personne.setPrenoms(personneVo.getPrenoms());
+        personne.setAge(personneVo.getAge());
+
+        personneRepository.save(personne);
+
+        jsonObject.put("status", HttpStatus.OK.value());
+        jsonObject.put("message", "personne enregistré avec succès");
+
+        return jsonObject.toString();
     }
 
     @Override
     public String updatePersonne(Long id, PersonneVO personneVo) {
+        JSONObject jsonObject = new JSONObject();
+        Optional<Personne> personne = personneRepository.findById(id);
+        if(!personne.isPresent()) {
+            jsonObject.put("status", HttpStatus.NOT_FOUND.value());
+            jsonObject.put("message", "impossible de MAJ une personne qui n'existe pas!");
+            return jsonObject.toString();
+        }
         
-        throw new UnsupportedOperationException("Unimplemented method 'updatePersonne'");
+        personne.get().setNom(personneVo.getNom());
+        personne.get().setPrenoms(personneVo.getPrenoms());
+        personne.get().setAge(personneVo.getAge());
+
+        personneRepository.save(personne.get());
+
+        jsonObject.put("status", HttpStatus.OK.value());
+        jsonObject.put("message", "personne MAJ avec succès");
+
+        return jsonObject.toString();
     }
 
     @Override
     public String fetchAllPersonne() {
         
-        throw new UnsupportedOperationException("Unimplemented method 'fetchAllPersonne'");
+        JSONObject jsonObject;
+        JSONArray jsonArray = new JSONArray();
+
+        List<Personne> personnes = personneRepository.findAll();
+
+        for(Personne personne : personnes) {
+            jsonObject = new JSONObject();
+            jsonObject.put("id",personne.getId());
+            jsonObject.put("nom", personne.getNom());
+            jsonObject.put("prenoms", personne.getPrenoms());
+            jsonObject.put("age", personne.getAge());
+            jsonArray.put(jsonObject);
+        }
+
+        jsonObject = new JSONObject();
+
+        jsonObject.put("status", HttpStatus.OK.value());
+        jsonObject.put("message", "succèss");
+        jsonObject.put("data", jsonArray);
+
+        return jsonObject.toString();
     }
 
     @Override
     public String fetchPersonne(Long id) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'fetchPersonne'");
+        JSONObject jsonObject = new JSONObject();
+        Optional<Personne> personne = personneRepository.findById(id);
+
+        if(!personne.isPresent()) {
+            jsonObject.put("status", HttpStatus.NOT_FOUND.value());
+            jsonObject.put("message", "personne introuvable");
+            return jsonObject.toString();
+        }
+
+        jsonObject.put("status", HttpStatus.OK.value());
+        jsonObject.put("message", "personne trouvé");
+        jsonObject.put("data", personne.get().getJson());
+
+        return jsonObject.toString();
     }
 
     @Override
     public String deletePersonne(Long id) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'deletePersonne'");
+        JSONObject jsonObject = new JSONObject();
+        Optional<Personne> personne = personneRepository.findById(id);
+        if(!personne.isPresent()) {
+            jsonObject.put("status", HttpStatus.NOT_FOUND.value());
+            jsonObject.put("message", "personne impossible à trouvé");
+            return jsonObject.toString();
+        }
+
+        personneRepository.delete(personne.get());
+
+        jsonObject.put("status", HttpStatus.OK.value());
+        jsonObject.put("message", personne.get().getNom() + " " + personne.get().getPrenoms() + " supprimé avec succèss");
+        return jsonObject.toString();
     }
     
 }
