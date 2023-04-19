8ea4bb9313464da32f022e4644f492d27fa281e0
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-04-19 01:06:54 +0000

    deepaete

--- src/main/java/com/eburtis/tp/interfaces/DepartementController.java
+++ src/main/java/com/eburtis/tp/interfaces/DepartementController.java
@@ -2,12 +2,14 @@ package com.eburtis.tp.interfaces;
 
 import com.eburtis.tp.application.departement.DepartementService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/departement")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DepartementController {
 
     @Autowired
