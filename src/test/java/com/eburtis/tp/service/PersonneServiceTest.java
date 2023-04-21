c8cb249b5a83b694050470144799a1212f35af88
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-04-21 10:03:33 +0000

    test unitaire terminé

--- src/test/java/com/eburtis/tp/service/PersonneServiceTest.java
+++ src/test/java/com/eburtis/tp/service/PersonneServiceTest.java
@@ -3,10 +3,13 @@ package com.eburtis.tp.service;
 import com.eburtis.tp.application.personne.PersonneService;
 import com.eburtis.tp.application.personne.PersonneVO;
 import com.eburtis.tp.domains.departement.Departement;
-import com.eburtis.tp.domains.personne.Personne;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -28,12 +31,6 @@ public class PersonneServiceTest {
         personneVO.setPrenoms("ezekiel");
         personneVO.setAge(21L);
         personneVO.setDepartement(new Departement(1L, "TY859663", "Departement informatique"));
-
-        Personne personne = new Personne();
-        personne.setNom(personneVO.getNom());
-        personne.setPrenoms(personneVO.getPrenoms());
-        personne.setAge(personne.getAge());
-        personne.setDepartement(personneVO.getDepartement());
 
         // WHEN
         String result = personneService.savePersonne(personneVO);
@@ -57,6 +54,38 @@ public class PersonneServiceTest {
 
         // WHEN
         String result = personneService.updatePersonne(id, personneVO);
+
+        // THEN
+        assertEquals(result, actual);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("test pour la liste des personnes")
+    public void testListePersonne() throws JsonProcessingException {
+        // GIVEN
+        String actual = "{\"data\":\"...\",\"status\":200}";
+        Gson gsonActual = new Gson();
+        Map<String, Object> mapActual = gsonActual.fromJson(actual, Map.class);
+
+        // WHEN
+        String result = personneService.fetchAllPersonne();
+        Gson gsonResult = new Gson();
+        Map<String, Object> mapResult = gsonResult.fromJson(result, Map.class);
+
+        // THEN
+        assertEquals(mapActual.get("status"), mapResult.get("status"));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("test pour la suppression d'une personne")
+    public void testSupprimerPresonne() {
+        // GIVEN
+        String actual = "{\"message\":\"personne impossible à trouvé\",\"status\":404}";
+
+        // WHEN
+        String result = personneService.deletePersonne(22L);
 
         // THEN
         assertEquals(result, actual);
